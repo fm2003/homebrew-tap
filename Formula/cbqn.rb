b031cd84fb3554c0c23e0dfe309f18f324aed4c1
fm2003 <fm2003@users.noreply.github.com>
2026-06-28 23:23:54 +0200

    Fix

--- Formula/cbqn.rb
+++ Formula/cbqn.rb
@@ -1,13 +1,10 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://docs.brew.sh/rubydoc/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Cbqn < Formula
-  desc "A BQN implementation in C"
+  desc "BQN implementation in C"
   homepage "https://github.com/dzaima/CBQN"
   url "https://github.com/dzaima/CBQN/archive/refs/tags/v0.11.0.tar.gz"
   sha256 "b8b680b9c4a26f67a3949f28f58604ae1c52ef8d5d5d97d521393c528c36b178"
-  head "https://github.com/dzaima/CBQN.git", branch: "develop"
   license "GPL-3.0-or-later"
+  head "https://github.com/dzaima/CBQN.git", branch: "develop"
 
   depends_on "pkgconf" => :build
   depends_on "libffi"
